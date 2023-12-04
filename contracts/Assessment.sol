b2a7a51962652ef45460756160767ceb146f948a
Vignesh8763 <151425234+Vignesh8763@users.noreply.github.com>
2023-12-04 23:53:55 +0530

    Update Assessment.sol

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -1,46 +1,44 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-//import "hardhat/console.sol";
-
 contract Assessment {
     address payable public owner;
     uint256 public balance;
 
     event Deposit(uint256 amount);
     event Withdraw(uint256 amount);
+    event NotificationSent(address indexed recipient, string message);
+    event EvenSquareCalculated(uint256 input, uint256 result);
+
+    string[] public notifications;
 
     constructor(uint initBalance) payable {
         owner = payable(msg.sender);
         balance = initBalance;
     }
 
-    function getBalance() public view returns(uint256){
+    function getBalance() public view returns (uint256) {
         return balance;
     }
 
     function deposit(uint256 _amount) public payable {
-        uint _previousBalance = balance;
+        uint256 _previousBalance = balance;
 
-        // make sure this is the owner
         require(msg.sender == owner, "You are not the owner of this account");
 
-        // perform transaction
         balance += _amount;
 
-        // assert transaction completed successfully
         assert(balance == _previousBalance + _amount);
 
-        // emit the event
         emit Deposit(_amount);
     }
 
-    // custom error
     error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
 
     function withdraw(uint256 _withdrawAmount) public {
         require(msg.sender == owner, "You are not the owner of this account");
-        uint _previousBalance = balance;
+        uint256 _previousBalance = balance;
+
         if (balance < _withdrawAmount) {
             revert InsufficientBalance({
                 balance: balance,
@@ -48,13 +46,43 @@ contract Assessment {
             });
         }
 
-        // withdraw the given amount
         balance -= _withdrawAmount;
 
-        // assert the balance is correct
         assert(balance == (_previousBalance - _withdrawAmount));
 
-        // emit the event
         emit Withdraw(_withdrawAmount);
+    }
+
+    function sendNotification(address _recipient, string memory _message) public {
+        require(msg.sender == owner, "You are not the owner of this account");
+
+        notifications.push(_message);
+
+        emit NotificationSent(_recipient, _message);
+    }
+
+    function getAllNotifications() public view returns (string memory) {
+        require(msg.sender == owner, "You are not the owner of this account");
+
+        string memory allNotifications;
+        for (uint256 i = 0; i < notifications.length; i++) {
+            allNotifications = string(abi.encodePacked(allNotifications, notifications[i], "\n"));
+        }
+
+        return allNotifications;
+    }
+
+    function calculateEvenSquare(uint256 _number) public returns (uint256) {
+        require(msg.sender == owner, "You are not the owner of this account");
+
+        
+        uint256 result = 0;
+        if (_number % 2 == 0) {
+            result = _number * _number;
+        }
+
+        emit EvenSquareCalculated(_number, result);
+
+        return result;
     }
 }
